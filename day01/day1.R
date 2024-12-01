9d0887887fcad6345cc52fc116bc01a9767370d0
Thomson Nguyen <thomson@cantab.net>
2024-12-01 14:15:35 -0800

    clean up whitespace

--- day01/day1.R
+++ day01/day1.R
@@ -1,6 +1,5 @@
 # https://adventofcode.com/2024/day/1
 
-
 input <- read.csv("input.aoc", header=FALSE, sep='', 
                   col.names = c('left','right'))
 left <- input$left[order(input$left, decreasing=FALSE)]
