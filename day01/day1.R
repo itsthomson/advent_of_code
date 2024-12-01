38fc15674834b2afe8bdb108499aaaf605fd68b5
Thomson Nguyen <thomson@cantab.net>
2024-12-01 14:41:14 -0800

    finish day 1

--- day01/day1.R
+++ day01/day1.R
@@ -1,7 +1,14 @@
 # https://adventofcode.com/2024/day/1
 
-input <- read.csv("input.aoc", header=FALSE, sep='', 
-                  col.names = c('left','right'))
+# Part 1
+input <- read.csv("input.aoc", header=FALSE, sep='', col.names = c('left','right'))
 left <- input$left[order(input$left, decreasing=FALSE)]
 right <- input$right[order(input$right, decreasing=FALSE)]
 abs(sum(left-right))
+
+# Part 2
+rolling_sum <- 0
+for (x in left){
+  rolling_sum <- rolling_sum + sum(x==right)*x
+}
+rolling_sum
